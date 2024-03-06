ac06b5a30261f0c26dbe58dfe3622eb04ff3edca
Gonzalo Hernandez <hgonzalo2000@gmail.com>
2024-03-06 00:18:33 -0300

    reading dataset from movilens and benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,96 @@
 use core::f64;
+use ndarray::{Array2, Axis};
+use std::{fs::read_to_string, time::Instant};
 
-use ndarray::{arr2, Array2, Axis};
-// Cargar todos los datos de MovieLens
+fn mse(
+    y: &Array2<f64>,
+    u: &Array2<f64>,
+    v: &Array2<f64>,
+    mask: &Array2<f64>,
+    count: &f64,
+) -> (Array2<f64>, f64) {
+    let difference = (y - u.dot(&v.t())) * mask;
+    let error = 0.5 * (difference.map_axis(Axis(1), |x| x.dot(&x))).sum() / count;
 
-fn mse(y: Array2<f64>, u: Array2<f64>, v: Array2<f64>) {
-    let difference = y - u.dot(&v.t());
-    let count = 20.;
-    let error = (difference.map_axis(Axis(1), |v| v.dot(&v))).sum() / count;
-    println!("Difference =\n{}\nError = {}", difference, error);
+    return (difference, error);
+}
+
+fn sgd(
+    y: Array2<f64>,
+    k_latent: usize,
+    alpha: f64,
+    max_steps: usize,
+    threshold: f64,
+) -> (Array2<f64>, f64) {
+    let mask = y.map(|x| {
+        if *x == 0. {
+            return 0.;
+        };
+        return 1.;
+    });
+    let (m, n) = y.dim();
+    let count = &mask.sum();
+    let mut r_squared = 0.;
+
+    let mut u = Array2::from_elem((m, k_latent), 3.5);
+    let mut v = Array2::from_elem((n, k_latent), 3.5);
+
+    for i in 0..max_steps {
+        let (difference, error) = mse(&y, &u, &v, &mask, &count);
+        r_squared = 1. - error;
+
+        if error < threshold {
+            println!("Threshold reached in {} iterations!", i);
+            break;
+        }
+
+        let u_delta = alpha / count * (difference.dot(&v));
+        let v_delta = alpha / count * (difference.t().dot(&u));
+
+        u = u + u_delta;
+        v = v + v_delta;
+    }
+
+    return (u.dot(&v.t()), r_squared);
+}
+
+fn read_movielens_data(datasource: &str) -> Array2<f64> {
+    let mut matrix = Array2::from_elem((943, 1682), 0.);
+
+    let lines: Vec<String> = read_to_string(datasource)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect();
+
+    for line in lines {
+        let mut iterator = line.split_whitespace().map(parse_to_float);
+
+        let row_num = iterator.next().unwrap() - 1;
+        let col_num = iterator.next().unwrap() - 1;
+        matrix[(row_num, col_num)] = iterator.next().unwrap() as f64;
+    }
+
+    return matrix;
+}
+
+fn parse_to_float(s: &str) -> usize {
+    return s.parse::<usize>().unwrap();
 }
 
 fn main() {
-    let test = arr2(&[
-        [4., 1., 2., 0., 5.],
-        [5., 2., 2.5, 3.5, 5.],
-        [1., 4., 4., 2., 5.],
-        [0., 3.5, 3., 1.5, 4.],
-    ]);
+    println!("Execution Started!");
+    let now = Instant::now();
+    let data = read_movielens_data("u.data");
 
-    let u_initial = arr2(&[[3., 3., 3.], [3., 3., 3.], [3., 3., 3.], [3., 3., 3.]]);
+    println!("Data fetched in {}ms!", now.elapsed().as_millis());
 
-    let v_initial = arr2(&[
-        [3., 3., 3.],
-        [3., 3., 3.],
-        [3., 3., 3.],
-        [3., 3., 3.],
-        [3., 3., 3.],
-    ]);
+    let (prediction, r_squared) = sgd(data, 5, 0.2, 1000, 0.9);
 
-    mse(test, u_initial, v_initial);
+    println!(
+        "Prediction Matrix:\n{}\nR_Squared = {}",
+        prediction, r_squared
+    );
+
+    println!("Successfully finished in {}s!", now.elapsed().as_secs());
 }
