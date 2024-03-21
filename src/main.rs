f42f2f92ea789b611f23a772b835af0e15aec99f
Gonzalo Hernandez <hgonzalo2000@gmail.com>
2024-03-21 15:29:58 -0300

    removing gd

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let movielens_reader = DatasourceReader::new("movielens_ds.data");
     let data = movielens_reader.read_data();
 
-    let mf = MF::new(0.2, 100, 5, 0.1);
+    let mf = MF::new(0.02, 100000, 5, 0.7);
     mf.train(data);
 
     println!("Successfully finished in {}s!", now.elapsed().as_secs());
--- src/mf.rs
+++ src/mf.rs
@@ -1,5 +1,4 @@
-use ndarray::{s, Array2, Axis};
-use rand::Rng;
+use ndarray::Array2;
 use std::time::Instant;
 
 pub struct MF {
@@ -19,8 +18,11 @@ impl MF {
         }
     }
 
-    // Not yet SGD bud GD
-    fn gd(&self, y: Array2<f64>) -> (Array2<f64>, f64) {
+    fn sgd(&self, y: Array2<f64>) -> (Array2<f64>, f64) {
+        let (m, n) = y.dim();
+        let mut u = Array2::from_elem((m, self.k_latent), 3.);
+        let mut v = Array2::from_elem((n, self.k_latent), 3.);
+
         let mask = y.map(|x| {
             if *x == 0. {
                 return 0.;
@@ -28,79 +30,47 @@ impl MF {
             return 1.;
         });
 
-        let (m, n) = y.dim();
-        let mut u = Array2::from_elem((m, self.k_latent), 3.);
-        let mut v = Array2::from_elem((n, self.k_latent), 3.);
-        let mut mean_squared_error = 0.;
+        for epoch in 0..self.max_steps {
+            for row in 0..m {
+                for col in 0..n {
+                    let rating = y[[row, col]];
+                    if rating != 0. {
+                        let user_row = u.row(row);
+                        let movie_col = v.row(col);
+                        let prediction = user_row.dot(&movie_col);
+                        let e = rating - prediction;
 
-        for i in 0..self.max_steps {
-            let estimation = u.dot(&v.t());
-            let (difference, error) = mse(&y, &estimation, &mask);
+                        let delta_u = self.alpha * e * movie_col.to_owned();
+                        let delta_v = self.alpha * e * user_row.to_owned();
 
-            mean_squared_error = error;
+                        let u_sum = user_row.to_owned() + delta_u;
+                        let v_sum = movie_col.to_owned() + delta_v;
 
-            if mean_squared_error <= self.threshold {
-                println!("Threshold reached in {} iterations!", i);
-                break;
+                        u.row_mut(row).assign(&u_sum);
+                        v.row_mut(col).assign(&v_sum);
+                    }
+                }
             }
 
-            // let delta_u = (2. * self.alpha / mask.sum()) * difference.dot(&v);
-            // let delta_v = (2. * self.alpha / mask.sum()) * difference.t().dot(&u);
-            let delta_u = 2. * self.alpha * error * &v;
-            let delta_v = 2. * self.alpha * error * &u;
-            println!("{:?}", delta_u.shape());
-            println!("{:?}", delta_v.shape());
+            let (_diff, _error, rerror) = mse(&y, &u.dot(&v.t()), &mask);
+            println!("Relative error = {} in epoch: {}", rerror, epoch);
 
-            u = u + &delta_u;
-            v = v + &delta_v;
-        }
-
-        return (u.dot(&v.t()), mean_squared_error);
-    }
-
-    fn sgd(&self, y: Array2<f64>) -> (Array2<f64>, f64) {
-        let (m, n) = y.dim();
-        let mut u = Array2::from_elem((m, self.k_latent), 3.);
-        let mut v = Array2::from_elem((n, self.k_latent), 3.);
-        let mut mean_squared_error = 0.;
-
-        for i in 0..self.max_steps {
-            let estimation = u.dot(&v.t());
-            let rand_number = rand::thread_rng().gen_range(0..(y.len_of(Axis(0))));
-            let batch_sample = y.slice(s![rand_number..rand_number + 10, ..]);
-
-            let mask = batch_sample.map(|x| {
-                if *x == 0. {
-                    return 0.;
-                };
-                return 1.;
-            });
-
-            let error = mse_v2(&y, &estimation, &mask);
-
-            mean_squared_error = error;
-
-            if mean_squared_error <= self.threshold {
-                println!("Threshold reached in {} iterations!", i);
+            if rerror < self.threshold {
+                println!("Threshold reached in {} iterations!", epoch);
                 break;
             }
-
-            let delta_u = 2. * self.alpha * error * &v;
-            let delta_v = 2. * self.alpha * error * &u;
-
-            u = u + &delta_u;
-            v = v + &delta_v;
         }
 
-        return (u.dot(&v.t()), mean_squared_error);
+        let final_prediction = u.dot(&v.t());
+        let (_diff, error, _rerror) = mse(&y, &final_prediction, &mask);
+        return (final_prediction, error);
     }
 
     pub fn train(&self, y: Array2<f64>) {
         let now = Instant::now();
         println!("Training model...");
 
-        let (estimation, error) = self.gd(y);
-        // let (estimation, error) = self.sgd(y);
+        let (estimation, error) = self.sgd(y);
         let prediction = round_all(&estimation);
 
         println!("MSE = {}\n{}", error, prediction);
@@ -108,88 +78,16 @@ impl MF {
     }
 }
 
-fn mse(y: &Array2<f64>, y_hat: &Array2<f64>, mask: &Array2<f64>) -> (Array2<f64>, f64) {
+fn mse(y: &Array2<f64>, y_hat: &Array2<f64>, mask: &Array2<f64>) -> (Array2<f64>, f64, f64) {
     let difference = (y - y_hat) * mask;
     let squared_difference = &difference * &difference;
     let error = squared_difference.sum() / (mask.sum() as f64);
+    let relative_to = (y * y).sum();
+    let rerror = squared_difference.sum() / relative_to;
 
-    return (difference, error as f64);
-}
-
-fn mse_v2(y: &Array2<f64>, y_hat: &Array2<f64>, mask: &Array2<f64>) -> f64 {
-    let difference = (y - y_hat) * mask;
-    let squared_difference = &difference * &difference;
-    let error = squared_difference.sum() / (y_hat.len() as f64);
-
-    return error;
+    return (difference, error, rerror);
 }
 
 fn round_all(y: &Array2<f64>) -> Array2<f64> {
     y.map(|x| x.round())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ndarray::arr2;
-
-    #[test]
-    fn test_mse_is_cero_when_estimation_is_equal_to_observed_data() {
-        let y = Array2::from_elem((4, 1), 1.);
-        let y_hat = Array2::from_elem((4, 1), 1.);
-        let mask = Array2::from_elem((4, 1), 1.);
-
-        let (_, error) = mse(&y, &y_hat, &mask);
-        assert_eq!(0., error);
-    }
-
-    #[test]
-    fn test_mse_is_non_zero_when_whole_estimation_is_different_from_observed_data() {
-        let y = arr2(&[[1., 1., 1., 1.], [1., 1., 1., 1.]]);
-        let y_hat = arr2(&[[0., 0., 0., 0.], [0., 0., 0., 0.]]);
-        let mask = Array2::from_elem((2, 4), 1.);
-
-        let (_, error) = mse(&y, &y_hat, &mask);
-        assert_eq!(1., error);
-    }
-
-    #[test]
-    fn test_non_negative_numbers_as_result_of_the_power() {
-        let y = arr2(&[[0., 0., 0., 0.], [0., 0., 0., 0.]]);
-        let y_hat = arr2(&[[1., 1., 1., 1.], [1., 1., 1., 1.]]);
-        let mask = Array2::from_elem((2, 4), 1.);
-
-        let (_, error) = mse(&y, &y_hat, &mask);
-        assert_eq!(1., error);
-    }
-
-    #[test]
-    fn test_mse_is_calculated_correctly_with_complex_example() {
-        let y = arr2(&[
-            [0., 10., 7., 5.],
-            [0., 2., 9., 12.],
-            [4., 2., 2., 6.],
-            [10., 3., 9., 15.],
-        ]);
-        let y_hat = arr2(&[
-            [2., 4., 10., 5.],
-            [0., 1., 6., 1.],
-            [3., 6., 2., 6.],
-            [21., 3., 14., 14.],
-        ]);
-        let mask = Array2::from_elem((4, 4), 1.);
-
-        let (_, error) = mse(&y, &y_hat, &mask);
-        assert_eq!(21.5, error);
-    }
-
-    #[test]
-    fn test_masked_values_are_not_considered_for_the_calculation() {
-        let y = arr2(&[[1., 1., 1., 1.], [1., 1., 1., 1.]]);
-        let y_hat = arr2(&[[1., 0., 0., 1.], [1., 1., 1., 1.]]);
-        let mask = arr2(&[[1., 0., 0., 1.], [1., 1., 1., 1.]]);
-
-        let (_, error) = mse(&y, &y_hat, &mask);
-        assert_eq!(0., error);
-    }
 }
